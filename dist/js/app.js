50b43a997fb4589c1a514962aa1627731e925ef4
Mohamed-Waled <80546573+Mohamed-Waled@users.noreply.github.com>
2022-12-17 12:40:03 +0200

    test

---
+++ dist/js/app.js
@@ -0,0 +1,68 @@
+function menuButton() {
+  let menuButton = document.querySelector(".menu-button");
+  let menuButtonTop = document.querySelector(".menu-button .top");
+  let menuButtonCenter = document.querySelector(".menu-button .center");
+  menuButton.addEventListener("click", () => {
+    let mobileMenu = document.querySelector(".mobile-menu");
+    if (!mobileMenu.classList.contains("active")) {
+      mobileMenu.style.display = "block";
+      mobileMenu.classList.add("active");
+      menuButton.style.transform = "rotate(45deg)";
+      menuButtonCenter.style.display = "none";
+      menuButtonTop.style.transform = "rotate(-90deg) translateX(-28%)";
+    } else {
+      mobileMenu.style.display = "none";
+      mobileMenu.classList.remove("active");
+      menuButton.style.transform = "unset";
+      menuButtonCenter.style.display = "block";
+      menuButtonTop.style.transform = "unset";
+    }
+  });
+}
+
+function onScroll() {
+  let header = document.querySelector("#header");
+  let about = document.querySelector("#about");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY >= about.scrollHeight - 400) {
+      header.style.boxShadow = "0px 0px 20px 0px #08111f";
+      header.style.background = "rgba(0, 0, 0, 0.1)";
+      header.style.backdropFilter = "blur(5px)";
+    } else {
+      header.style.boxShadow = "unset";
+      header.style.background = "unset";
+      header.style.backdropFilter = "unset";
+    }
+  });
+}
+
+function tabs() {
+  let switcherLis = document.querySelectorAll(".tabs li");
+  let allProjects = document.querySelectorAll(".all");
+
+  switcherLis.forEach((li) => {
+    li.addEventListener("click", removeActive);
+    li.addEventListener("click", filter);
+  });
+
+  function removeActive() {
+    switcherLis.forEach((li) => {
+      li.classList.remove("active");
+      this.classList.add("active");
+    });
+  }
+
+  function filter() {
+    allProjects.forEach((project) => {
+      project.style.display = "none";
+    });
+    document.querySelectorAll(this.dataset.cat).forEach((project) => {
+      project.style.display = "flex";
+    });
+  }
+}
+
+menuButton();
+onScroll();
+tabs();
